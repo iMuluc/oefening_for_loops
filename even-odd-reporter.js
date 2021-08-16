555805e9e3e1ed9a2a85ba4f9d9dc4472cf8f637
iMuluc <iMertUluc@gmail.com>
2021-08-16 02:14:14 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    if (i % 2 == 0) {
+        console.log(i + ' is even')
+        console.log('');
+    } else {
+        console.log(i + ' is odd')
+        console.log('');
+    }
+};
+
---
+++ multiplication-tables.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <= 10; i++) {
+    let j = 9;
+    let k = i * j;
+    console.log(i + ' x ' + j + ' = ' + k);
+    console.log('');
+}
